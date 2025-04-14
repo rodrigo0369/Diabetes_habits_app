1d11e6b7bac1f674f0be4bb70ba03f818a22c304
rodrigo0369 <rodrigoirala18@gmail.com>
2025-04-14 15:25:13 -0300

    Create es.dart

---
+++ assets/i18n/es.dart
@@ -0,0 +1,21 @@
+part of '../app_localizations.dart';
+
+class Es extends Translations {
+  const Es();
+
+  @override
+  Map<String, dynamic> get keys => {
+        'title': 'Título',
+        'habits': 'Hábitos',
+        'glucosa': 'Glucosa',
+        'configuracion': 'Configuración',
+        'add_habit': 'Agregar Hábito',
+        'reminder': 'Recordatorio',
+        'select_reminder': 'Seleccionar Recordatorio',
+        'cancel': 'Cancelar',
+        'save': 'Guardar',
+        'habits_completed_today': 'Hábitos completados hoy',
+        'please_enter_title': 'Por favor, ingresa un título',
+        // ... (Añade todas las claves que uses)
+      };
+}
