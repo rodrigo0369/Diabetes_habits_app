a9f3bc6b6e413eedee7c7f341d22f8410bc13073
rodrigo0369 <rodrigoirala18@gmail.com>
2025-04-14 14:45:56 -0300

    Create Recommendations_data.dart

---
+++ lib/utils/Recommendations_data.dart
@@ -0,0 +1,21 @@
+//  Este archivo contendrá las recomendaciones.
+//  En una app real, esto podría venir de una base de datos o API.
+
+const List<Map<String, dynamic>> recommendationsData = [
+  {
+    'content': 'Para el Tipo 1 en Argentina: Controla tus niveles de glucosa frecuentemente.',
+    'diabetesType': 'Tipo 1',
+    'country': 'Argentina',
+  },
+  {
+    'content': 'Para el Tipo 2 en México: Incluye más nopales en tu dieta.',
+    'diabetesType': 'Tipo 2',
+    'country': 'México',
+  },
+  {
+    'content': 'Para el Tipo Gestacional en España: Camina después de las comidas.',
+    'diabetesType': 'Gestacional',
+    'country': 'España',
+  },
+  //  Añade más recomendaciones aquí...
+];
